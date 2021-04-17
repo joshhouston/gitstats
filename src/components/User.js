1589ef9f65249c88c563d36cffa273d300b728ae
josh <jjhouston24@gmail.com>
2021-04-17 12:11:13 -0500

    displayed number of users repos, followers, following

--- src/components/User.js
+++ src/components/User.js
@@ -1,44 +1,43 @@
 import React, { useState, useEffect } from 'react';
-import {format, parseISO} from 'date-fns';
+import { format, parseISO } from 'date-fns';
 import Github from 'github-api';
 
 
 const gh = new Github();
 
-function User({username}) {
+function User({ username }) {
     const [isLoading, setLoading] = useState(true)
     const [repo, setRepo] = useState([])
     const [profile, setProfile] = useState({})
     const [date, setDate] = useState('')
-    
+
     let user = gh.getUser(`${username}`)
-    
-    
-    useEffect( () => {
+
+
+    useEffect(() => {
         async function fetchData() {
             const result = await user.listStarredRepos()
             setRepo([...result.data])
-            
         }
 
         async function getProfile() {
             const result = await user.getProfile()
-            
-            let date = format(parseISO(result.data.created_at), "MMMM d, yyyy") 
+            console.log(result.data)
+            let date = format(parseISO(result.data.created_at), "MMMM d, yyyy")
 
             setDate(date)
-            setProfile({...result.data})
+            setProfile({ ...result.data })
             setLoading(false)
         }
 
         fetchData();
         getProfile();
 
-        
-      
-  }, [])
 
-    
+
+    }, [])
+
+
     return (
         <div>
             { isLoading ? (
@@ -47,12 +46,28 @@ function User({username}) {
                 </div>
 
             )
-             : (
-                <div>
-                    <img src={profile.avatar_url} alt=""/>
-                    <p>Joined {date} </p>
-                </div>
-             )
+                : (
+                    <div>
+                        <img src={profile.avatar_url} alt="" />
+                        <p>Joined {date} </p>
+                        
+                        <div className="overview">
+                            <div className="overview-repos">
+                                <h4>{profile.public_repos}</h4>
+                                <p>Repositories</p>
+                            </div>
+                            <div className="overview-followers">
+                            <h4>{profile.followers}</h4>
+                                <p>Followers</p>
+                            </div>
+                            <div className="overview-following">
+                            <h4>{profile.following}</h4>
+                                <p>Following</p>
+                            </div>
+
+                        </div>
+                    </div>
+                )
             }
         </div>
     )
