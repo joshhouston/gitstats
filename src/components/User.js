903d03d539218c54a7b10499c52d08624426ac55
josh <jjhouston24@gmail.com>
2021-04-17 12:05:40 -0500

    obtained users profile image and date joined

--- src/components/User.js
+++ src/components/User.js
@@ -1,31 +1,42 @@
 import React, { useState, useEffect } from 'react';
+import {format, parseISO} from 'date-fns';
 import Github from 'github-api';
 
 
-const gh = new Github({
-  username: process.env.REACT_APP_USERNAME,
-  password: process.env.REACT_APP_PASSWORD
-});
+const gh = new Github();
 
 function User({username}) {
     const [isLoading, setLoading] = useState(true)
     const [repo, setRepo] = useState([])
+    const [profile, setProfile] = useState({})
+    const [date, setDate] = useState('')
     
     let user = gh.getUser(`${username}`)
     
+    
     useEffect( () => {
-
         async function fetchData() {
             const result = await user.listStarredRepos()
             setRepo([...result.data])
-            setLoading(false)
             
         }
 
+        async function getProfile() {
+            const result = await user.getProfile()
+            
+            let date = format(parseISO(result.data.created_at), "MMMM d, yyyy") 
+
+            setDate(date)
+            setProfile({...result.data})
+            setLoading(false)
+        }
+
         fetchData();
-       
+        getProfile();
+
+        
       
-  }, [user])
+  }, [])
 
     
     return (
@@ -38,13 +49,8 @@ function User({username}) {
             )
              : (
                 <div>
-                    {repo.map((repos, id) => {
-                        return (
-                            <div key={id}>
-                                <p>{repos.name}</p> 
-                            </div>
-                        )
-                    }) }
+                    <img src={profile.avatar_url} alt=""/>
+                    <p>Joined {date} </p>
                 </div>
              )
             }
