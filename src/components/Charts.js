b06e8655d11bcde2a32d1d480843f66b8d3f1c53
josh <jjhouston24@gmail.com>
2021-04-30 19:15:51 -0500

    displayed users top languages with chart.js

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,17 +1,61 @@
 import React, {useState} from 'react';
+import {Doughnut} from 'react-chartjs-2';
 
 
 function Charts({repos}) {
+    let language = []
+    let counter = 0
+    
+    let data = {
+        labels: [],
+        datasets: [{
+            data: [],
+            backgroundColor: [
+                '#61DAFB',
+                '#DD1B16',
+                '#41B883'
+            ],
+            borderColor: [
+                '#61DAFB',
+                '#DD1B16',
+                '#41B883'
+            ],
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        }]
+    }
 
+    
+    repos
+        .filter(i => i.language)
+        .forEach(repo => {
+            const idx = data.labels.indexOf(repo.language)
+            if(idx !== -1){
+                data.datasets[0].data[idx]++
+            }else {
+                data.labels.push(repo.language)
+                data.datasets[0].data.push(1)
+                
+            }
+        })
+        
     console.log(repos)
+    // repos.map(repo => language.push(repo.language))
+
+    // repos.forEach((lang, i) => console.log(i) )
+    // console.log(language)
     return (
         <div>
-            {repos.map((repo, index) => (
-
-                <div key={index}>
-                    {repo.name}
-                </div>
-            ))}
+            <Doughnut
+                data={data}
+             />
         </div>
     )
 }
