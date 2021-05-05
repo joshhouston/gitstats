1031cb13230b9ecea33ad493a03fb5ed21f2733f
josh <jjhouston24@gmail.com>
2021-05-05 10:51:05 -0500

    created most starred bar chart

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,13 +1,15 @@
-import React, {useState} from 'react';
-import {Doughnut} from 'react-chartjs-2';
+import React from 'react';
+import { Doughnut, Pie, Bar } from 'react-chartjs-2';
 
 
-function Charts({repos}) {
-    let language = []
-    let counter = 0
-    
-    let data = {
+function Charts({ repos }) {
+
+    //Language Pie Chart Data
+
+    let pieData = {
         labels: [],
+        responsive: true,
+        maintainAspectRatio: false,
         datasets: [{
             data: [],
             backgroundColor: [
@@ -32,27 +34,60 @@ function Charts({repos}) {
         }]
     }
 
-    
+
     repos
         .filter(i => i.language)
         .forEach(repo => {
-            const idx = data.labels.indexOf(repo.language)
-            if(idx !== -1){
-                data.datasets[0].data[idx]++
-            }else {
-                data.labels.push(repo.language)
-                data.datasets[0].data.push(1)
-                
+            const idx = pieData.labels.indexOf(repo.language)
+            if (idx !== -1) {
+                pieData.datasets[0].data[idx]++
+            } else {
+                pieData.labels.push(repo.language)
+                pieData.datasets[0].data.push(1)
+
             }
         })
+
+    //Most Starred Bar Chart
+    const starred = repos.sort((a, b) => b.stargazers_count - a.stargazers_count);
+    
+    let barData = {
+        labels: starred.map(i => i.name).slice(0,5),
+        responsive: true,
+        maintainAspectRatio: false,
+        datasets: [{
+            data: starred.map(i => i.stargazers_count).slice(0,5),
+            backgroundColor: [
+                '#61DAFB',
+                '#DD1B16',
+                '#41B883'
+            ],
+            borderColor: [
+                '#61DAFB',
+                '#DD1B16',
+                '#41B883'
+            ],
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        }]
+    }
+    
+
+
         
     
-    return (
-        <div>
-            <Doughnut
-                data={data}
-             />
-        </div>
+    return(
+        <div className='charts'>
+        <Pie data={pieData} />
+        <Bar data={barData} />
+        </div >
     )
 }
 
